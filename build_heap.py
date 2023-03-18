aa35b4fbd80f4b430a52218642d3d7c598bb5cb1
Mareks Rozenblats <mareks.rozenblats@edu.rtu.lv>
2023-03-18 23:48:59 +0200

    new

--- build_heap.py
+++ build_heap.py
@@ -1,13 +1,31 @@
 # python3
 
+from heapq import heapify
+
 
 def build_heap(data):
+    n=len(data)
     swaps = []
     # TODO: Creat heap and heap sort
     # try to achieve  O(n) and not O(n2)
-
+    for i in range(n // 2,-1,-1):
+        heapify(data,i, swaps)
 
     return swaps
+
+def heapify(data,i, swaps):
+    n=len(data)
+    kreisais = 2*i +1
+    labais = 2*i+2
+    min = i
+    if kreisais < n and data[kreisais]<data[min]:
+        min = kreisais
+    if labais < n and data[kreisais]<data[min]:
+        min = labais
+    if  i != min:
+        data[i], data[min] = data[min], data[i]
+        swaps.append((i,min))
+        heapify(data,min,swaps)
 
 
 def main():
@@ -16,10 +34,20 @@ def main():
     # add another input for I or F 
     # first two tests are from keyboard, third test is from a file
 
-
+    text = input()
+    if "I" in text:
     # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
+        n = int(input())
+        data = list(map(int, input().split()))
+    if "F" in text:
+        fails = input()
+        if "a" in fails:
+            print("wrong file name")
+            return
+        with open("./test/" + input(), "r").read() as f:
+            n=int(f.readline())
+            data = list(map(int, f.readline().split()))
+    
 
     # checks if lenght of data is the same as the said lenght
     assert len(data) == n
