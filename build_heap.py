424f5eb4453d04b7e5228fda249f1ffdf8fe4d7c
Mareks Rozenblats <mareks.rozenblats@edu.rtu.lv>
2023-03-19 23:45:15 +0200

    new

--- build_heap.py
+++ build_heap.py
@@ -60,7 +60,8 @@ def main():
                 n=int(f.readline().strip())
                 data=f.readline()
                 data = list(map(int, data.split()))
-            
+                
+
     assert len(data) == n
     # checks if lenght of data is the same as the said lenght
     #if len(data) != n:
@@ -71,8 +72,9 @@ def main():
     # and give back all swaps
     swaps = build_heap(data)
 
-    if len(swaps) <= 4 * len(data):
-        assert len(swaps)
+    if len(swaps) > 4 * len(data):
+        print ("out of bound")
+        return
     # TODO: output how many swaps were made, 
     # this number should be less than 4n (less than 4*len(data))
 
